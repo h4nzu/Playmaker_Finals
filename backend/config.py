2af10f306330bf999bba9b4553eb81a82a72c3f5
Hanz Lorenzo Olvega <hanzplays104@gmail.com>
2026-05-30 00:59:00 +0800

    backend changes

--- backend/config.py
+++ backend/config.py
@@ -46,6 +46,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://playmaker-finals.vercel.app",
 ]
 
 # Logging Configuration
